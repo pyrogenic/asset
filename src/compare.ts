1d7cec7580e4411f6ab517274a1b5f8460a15a1e
Joshua Pollak <abottomlesspit@gmail.com>
2021-07-03 14:11:19 -0700

    compare with tostring

--- src/compare.ts
+++ src/compare.ts
@@ -1,9 +1,10 @@
-type Comparand = undefined | string | Comparand[];
-type Options = {
+type Comparand<T> = undefined | string | Comparand<T>[];
+type Options<T> = {
     emptyLast?: boolean;
+    toString?: (v: T) => Comparand<T>;
 }
 
-export function compare(a: Comparand, b: Comparand, options?: Options): number {
+export function compare<T>(a: Comparand<T>, b: Comparand<T>, options?: Options<T>): number {
     if (a === undefined) {
         if (b === undefined) {
             return 0;
@@ -12,8 +13,8 @@ export function compare(a: Comparand, b: Comparand, options?: Options): number {
     } else if (b === undefined) {
         return options?.emptyLast ? -1 : 1;
     }
-    if (Array.isArray(a)) {
-        if (Array.isArray(b)) {
+    if (arrayLike(a)) {
+        if (arrayLike(b)) {
             if (a.length === 0) {
                 if (b.length === 0) {
                     return 0;
@@ -27,16 +28,38 @@ export function compare(a: Comparand, b: Comparand, options?: Options): number {
                     // a is longer
                     return 1;
                 }
-                const r = compare(a[i], b[i]);
+                const r = compare(a[i], b[i], options);
                 if (r !== 0) {
                     return r;
                 }
             }
             return a.length === b.length ? 0 : -1;
         }
-        return compare(a, [b]);
-    } else if (Array.isArray(b)) {
-        return compare([a], b);
+        return compare(a, [b], options);
+    } else if (arrayLike(b)) {
+        return compare([a], b, options);
     }
-    return a.localeCompare(b);
+    if (typeof a === "string") {
+        if (typeof b === "string") {
+            return a.localeCompare(b);
+        } else if (options?.toString) {
+            return compare(a, options.toString(b), options);
+        }
+    } else if (typeof b === "string") {
+        if (options?.toString) {
+            return compare(options.toString(a), b, options);
+        }
+    } else if (options?.toString) {
+        return compare(options.toString(a), options.toString(b), options);
+    }
+    console.warn({ badOperands: { a, b }, ...(options ?? {}) });
+    return 0;
+}
+
+function arrayLike<T>(a: string | Comparand<T>[]): a is Comparand<T>[] {
+    return Array.isArray(a);
+    // if (typeof a === "object") {
+    //     return a.length !== undefined;
+    // }
+    // return false;
 }
